78fb4a17a53ae4730a6da429a7d1dbccb92f96eb
Philip Norman <phil.normandin@live.ca>
2018-06-27 20:35:08 -0400

    plusieurs updates

--- functions.php
+++ functions.php
@@ -4,6 +4,17 @@ function my_theme_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
+// set_post_thumbnail_size( 1175, 522, true );
+
+// add_image_size( 'event-featured-image', '1175', '522', [ "center", "center" ] );
+
+// add_filter( 'image_size_names_choose', 'my_custom_sizes' );
+// function my_custom_sizes( $sizes ) {
+// return array_merge( $sizes, array(
+// 'event-featured-image' => __( 'Event Featured Image' )
+// ) );
+// }
+
 // function polylang_flags_shortcode() {
 //     ob_start();
 //     pll_the_languages(array('show_flags'=>1,'show_names'=>0));
