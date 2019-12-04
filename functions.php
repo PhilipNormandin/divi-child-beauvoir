8d77c6bccd76f04c894cb31443801249e7886b80
Philip Norman <phil.normandin@live.ca>
2019-12-04 13:48:50 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -500,7 +500,6 @@ function show_photo_albums_page_link() {
         $link_url = get_permalink( get_page_by_path( 'photo-albums' ) );
     } else {
         $link_url = get_permalink( get_page_by_path( 'albums-photo' ) );
-        echo "string!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
     }
 
     $return_string = '<a href="' . $link_url . '">' . $link_text . '</a>';
