d06234b759fbeb0be5d3e334569f4d7db97d71fd
Philip Norman <phil.normandin@live.ca>
2019-12-04 10:16:29 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -494,10 +494,10 @@ function show_photo_albums_page_link() {
     $link_url = null;
     $link_text = __( 'Back to Photo gallery', 'divi-child-beauvoir' );
 
-    if ( $curlang == "en" ) {
+    if ( pll_current_language() == 'en' ) {
         $link_url = get_permalink( get_page_by_path( 'photo-gallery' ) );
         echo "english !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
-    } elseif ( $curlang == "fr" ) {
+    } elseif ( pll_current_language() == 'fr' ) {
         $link_url = get_permalink( get_page_by_path( 'albums-photo' ) );
         echo "français !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
     }
