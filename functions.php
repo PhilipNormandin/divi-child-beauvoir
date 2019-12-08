388a7ba99f7ebf15be8903cc49e7f61cc101f7cb
Philip Norman <phil.normandin@live.ca>
2019-12-08 11:56:51 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -601,7 +601,7 @@ function disable_wp_responsive_images() {
         return 1;
     }
 }
-add_filter( 'max_srcset_image_width', 'disable_wp_responsive_images' );
+// add_filter( 'max_srcset_image_width', 'disable_wp_responsive_images' );
 
 
 // Add image sizes
