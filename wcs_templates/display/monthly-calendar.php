90ac35214f3640aa505f2ae7370e1c7f2571fa8a
Philip Norman <phil.normandin@live.ca>
2019-12-09 10:33:33 -0500

    Update monthly-calendar.php

--- wcs_templates/display/monthly-calendar.php
+++ wcs_templates/display/monthly-calendar.php
@@ -63,7 +63,7 @@
 			<h4 class="wcs-day-agenda__title" v-if="getOption( 'mth_cal_date_format', 'MMMM DD' )">{{selectedDay.date | moment( getOption( 'mth_cal_date_format', 'MMMM DD' ), false) }}</h4>
 			<template v-if="getFilteredCalendarEvents(selectedDay.events).length > 0">
 			<div v-for="event in getFilteredCalendarEvents(selectedDay.events)" class="wcs-class" :class="event | eventCSS">
-				<div v-if="event.thumbnail" class='wcs-class__image wcs-modal-call' :style='"background-image: url(" + event.thumbnail +")"' v-on:click="openModal( event, options, $event )"></div>
+				<!-- <div v-if="event.thumbnail" class='wcs-class__image wcs-modal-call' :style='"background-image: url(" + event.thumbnail +")"' v-on:click="openModal( event, options, $event )"></div> -->
 				<div class="wcs-class__inner">
 					<div class="wcs-class__time-duration">
 						<span v-html="starting_ending(event)"></span>
@@ -78,26 +78,6 @@
 							<taxonomy-list :options="options" :tax="'wcs_instructor'" :event="event" v-on:open-modal="openTaxModal"></taxonomy-list>
 						</template>
 					</div>
-					<div class="wcs-class__action">
-						<div class="wcs-class__inner-flex">
-							<a v-if="hasModal(event) && options.label_info.length > 0" href="#" class="wcs-btn wcs-modal-call" v-on:click="openModal( event, options, $event )">{{options.label_info}}</a>
-							<a v-else-if="hasLink(event) && options.label_info.length > 0" :href="event.permalink" class="wcs-btn">{{options.label_info}}</a>
-							<template v-for="(button, button_type) in event.buttons">
-								<template v-if="button_type == 'main' && button.label.length > 0 ">
-									<a class="wcs-btn wcs-btn--action" v-if="button.method == 0" :href="button.permalink" :target="button.target ? '_blank' : '_self'">{{button.label}}</a>
-									<a class="wcs-btn wcs-btn--action" v-else-if="button.method == 1" :href="button.custom_url" :target="button.target ? '_blank' : '_self'">{{button.label}}</a>
-									<a class="wcs-btn wcs-btn--action" v-else-if="button.method == 2" :href="button.email" :target="button.target ? '_blank' : '_self'">{{button.label}}</a>
-									<a class="wcs-btn wcs-btn--action" v-else-if="button.method == 3" :href="button.ical" target="_blank">{{button.label}}</a>
-								</template>
-								<template v-else-if="button_type == 'woo'">
-									<a :class="button.classes" v-if="button.status" :href="button.href">{{button.label}}</a>
-									<a :class="button.classes" v-else-if="!button.status && button.href" :href="button.href">{{button.label}}</a>
-									<a :class="button.classes" v-else-if="!button.status" href="#">{{button.label}}</a>
-								</template>
-							</template>
-						</div>
-					</div>
-<?php echo "testing" ?>
 					<div v-if="filter_var(options.show_excerpt)" class="wcs-class__excerpt" v-html="event.excerpt"></div>
 				</div>
 			</div>
