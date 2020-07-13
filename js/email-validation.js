3fdeccd6774c8b17688a814ea984b633e1c2614d
Philip Norman <phil.normandin@live.ca>
2020-07-13 08:32:42 -0400

    Update email-validation.js

--- js/email-validation.js
+++ js/email-validation.js
@@ -1,4 +1,49 @@
-/* Divi Child Beauvoir - Custom Email Validation */
+/* Divi Child Beauvoir - Contact Form Scripts */
+
+
+/* =========================================
+        Animation du formulaire
+========================================= */
+
+
+(function($) {
+
+    $(` .et_pb_section .et_pb_contact .et_pb_contact_form  p:not([data-type="checkbox"]):not([data-type="radio"]) `).each(function() {
+        $(this).find('textarea').insertBefore($(this).find('label'));
+        $(this).find('input').insertBefore($(this).find('label'));
+    });
+
+    $(` .et_pb_contact .et_pb_contact_form  p:not([data-type="checkbox"]) input,
+        .et_pb_contact .et_pb_contact_form  p:not([data-type="checkbox"]) textarea `).focus(function() {
+        $(this).parent("p").addClass("focus");
+    });
+
+    $(` .et_pb_contact .et_pb_contact_form  p:not([data-type="checkbox"]) input,
+        .et_pb_contact .et_pb_contact_form  p:not([data-type="checkbox"]) textarea `).blur(function() {
+        if ($(this).val()) {
+            $(this).parent().addClass("filled");
+        } else {
+            $(this).parent().removeClass("filled");
+        }
+        $(this).parent("p").removeClass("focus");
+    });
+
+})(jQuery);
+
+// Détecte les champs remplis (suite à un rafraichissement de la page)
+jQuery( document ).ready( function( $ ) {
+    $(` .et_pb_contact .et_pb_contact_form  p:not([data-type="checkbox"]) input,
+        .et_pb_contact .et_pb_contact_form  p:not([data-type="checkbox"]) textarea `).each(function() {
+        if ($(this).val()) {
+            $(this).parent().addClass("filled");
+        }
+    });
+});
+
+
+/* =========================================
+         Validation du formulaire
+========================================= */
 
 
 jQuery( document ).ready( function( $ ) {
