2bfba598293f42f86124bad52d1e72b7ff39c336
Philip Norman <phil.normandin@live.ca>
2020-07-13 10:48:32 -0400

    Update email-validation.js

--- js/email-validation.js
+++ js/email-validation.js
@@ -83,12 +83,12 @@ jQuery( document ).ready( function( $ ) {
 
     // add image which will appear with thank you message
     var thanks_image = myGlobalObject.templateUrl + "/images/bird-1320792_640.png";
-    var form_width = $("#formulaire-colonne").css("outerWidth");
-    var form_height = $("#formulaire-colonne").css("outerHeight");
+    var form_width = $("#formulaire-colonne").css("innerWidth");
+    var form_height = $("#formulaire-colonne").css("innerHeight");
 
     $( "#my-contact-form" ).ajaxSuccess( function() {
-        $("#formulaire-colonne").width(form_width);
-        $("#formulaire-colonne").height(form_height);
+        $("#formulaire-colonne").innerWidth(form_width);
+        $("#formulaire-colonne").innerHeight(form_height);
         // $( "#formulaire-colonne h2" ).attr("style", "opacity:0");
         $( "#formulaire-colonne h2" ).fadeTo("fast", 0);
         $( ".et_pb_contact_form_container" ).append( "<img src=" + thanks_image + ">" );
