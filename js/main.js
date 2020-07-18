c3dae14eca98eb4da20458261439aede7551dab5
Philip Norman <phil.normandin@live.ca>
2020-07-18 14:58:01 -0400

    refactoring

--- js/main.js
+++ js/main.js
@@ -42,3 +42,53 @@ jQuery( document ).ready( function( $ ) {
     });
 
 });
+
+
+/* Active/Désactive animation lorsqu'on clique sur un blurb (sommaire dans la page Histoire du Sanctuaire)
+https://www.elegantthemes.com/blog/divi-resources/
+how-to-toggle-divi-transform-properties-on-click-with-jquery
+------------------------------------------------------------------------ */
+
+jQuery( document ).ready( function( $ ) {
+
+    $( '.transform_target' ).click( function() {
+        $( '#toc .et_pb_row' ).toggleClass( 'toggle-transform-animation' );
+    });
+
+    // referme le sommaire lorsqu'on clique sur un lien
+    $( '#toc a' ).click( function() {
+        setTimeout( function() {
+            $( '#toc .et_pb_row' ).toggleClass(' toggle-transform-animation' );
+        }, 1200);
+    });
+
+});
+
+
+/* https://divibooster.com/remove-the-leading-zero-on-countdown-module-days/
+------------------------------------------------------------------------ */
+
+jQuery( function( $ ){
+
+	var olddays = $( '.et_pb_countdown_timer .days .value' );
+
+	// Clone the days and hide the original.
+	// - Wraps new days element in a span to prevent Divi from updating it
+	olddays.each( function(){
+		var oldday = $(this);
+		oldday.after(oldday.clone());
+		oldday.next().wrap('<span></span>');
+	}).hide();
+
+	// Update the clone each second, removing the trailing zero
+	(function update_days() {
+		olddays.each(function(){
+			var oldday = $(this);
+			var days = oldday.html();
+			if (days.substr(0,1) == '0') { days = days.slice(1); }
+			oldday.next().find('.value').html(days);
+		});
+		setTimeout(function(){ update_days(); }, 1000);
+	})()
+
+});
