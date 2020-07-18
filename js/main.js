4a725ac049377890d2d4b4ae012404bd95b08e89
Philip Norman <phil.normandin@live.ca>
2020-07-18 10:02:53 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ jQuery( document ).ready( function( $ ) {
 
 jQuery( document ).ready( function( $ ) {
 
-    $( "#newsletter-form .et_bloom_submit_subscription" ).ajaxSuccess( function() {
+    $( "#newsletter-form form" ).ajaxSuccess( function() {
         $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
     }
 
