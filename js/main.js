395ae61b2a5cd54dac5c3989d283635b5391e1da
Philip Norman <phil.normandin@live.ca>
2020-07-18 10:16:36 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,20 +34,20 @@ jQuery( document ).ready( function( $ ) {
 
 jQuery( document ).ready( function( $ ) {
 
-    $( "#newsletter-form" ).ajaxSuccess( function() {
-        $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
-    }
+    // $( "#newsletter-form" ).ajaxSuccess( function() {
+    //     $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
+    // }
 
-    // $( document ).ajaxComplete( function( event, xhr, settings ) {
-    //
-    //     // alert(xhr.responseText);
-    //
-    //
-    //
-    //     // if ( xhr.responseText == '{"success":"success"}' ) {
-    //     //     $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
-    //     // }
-    // });
+    $( document ).ajaxComplete( function( event, xhr, settings ) {
+
+        // alert(xhr.responseText);
+
+
+
+        if ( xhr.responseText == '{"success":"success"}' ) {
+            $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
+        }
+    });
 
     // $("#bulletin-column form").bind('ajax:complete', function() {
     //     $( "#bulletin-column .et_pb_promo_description > h2" ).addClass("subscribed");
