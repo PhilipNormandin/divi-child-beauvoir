a667bd0276a5116428f44810a5ae8764f21ee22f
Philip Norman <phil.normandin@live.ca>
2020-07-18 14:38:48 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,35 +34,11 @@ jQuery( document ).ready( function( $ ) {
 
 jQuery( document ).ready( function( $ ) {
 
-    // $( "#newsletter-form" ).ajaxSuccess( function() {
-    //     $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
-    // }
-
-    $( "#newsletter-form .et_bloom_success_container" ).on('classChanged', function() {
-
-        $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
-    });
-
     $( document ).ajaxComplete( function( event, xhr, settings ) {
-        if ( settings.url === "bloomSettings.ajaxurl" ) {
-            alert("Ça marche!!!");
+
+        if ( xhr.responseText == '{"success":"success"}' ) {
+            $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
         }
-
-    }
-
-    // $( document ).ajaxComplete( function( event, xhr, settings ) {
-    //
-    //     // alert(xhr.responseText);
-    //
-    //
-    //
-    //     if ( xhr.responseText == '{"success":"success"}' ) {
-    //         $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
-    //     }
-    // });
-
-    // $("#bulletin-column form").bind('ajax:complete', function() {
-    //     $( "#bulletin-column .et_pb_promo_description > h2" ).addClass("subscribed");
-    // });
+    });
 
 });
