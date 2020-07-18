fa5c06d6f3564e1a7e01fe75195262d4ce286532
Philip Norman <phil.normandin@live.ca>
2020-07-18 09:38:51 -0400

    some refactoring

---
+++ archives/js/old-scripts.js
@@ -0,0 +1,14 @@
+/* Ajuste vitesse animation retour au début
+------------------------------------------------------------------------ */
+
+// jQuery( document ).ready( function( $ ) {
+//
+//     var scroll_top_btn = $('.et_pb_scroll_top');
+//
+//     scroll_top_btn.click(function(e) {
+//         e.preventDefault();
+//         var doc_scroll = $( window ).scrollTop();
+//         $('html, body').animate({scrollTop:0}, doc_scroll);
+//     });
+//
+// });
--- js/main.js
+++ js/main.js
@@ -13,22 +13,6 @@ jQuery( document ).ready( function( $ ) {
 });
 
 
-/* Ajuste vitesse animation retour au début
------------------------------------------------------------------------- */
-
-// jQuery( document ).ready( function( $ ) {
-//
-//     var scroll_top_btn = $('.et_pb_scroll_top');
-//
-//     scroll_top_btn.click(function(e) {
-//         e.preventDefault();
-//         var doc_scroll = $( window ).scrollTop();
-//         $('html, body').animate({scrollTop:0}, doc_scroll);
-//     });
-//
-// });
-
-
 /* Préserve fonction des flèches dans les champs de formulaire
   (le plugin The Grid prend le contrôle des touches gauche et droite, ce qui pose problème
    si le client veut les utiliser pour déplacer le curseur dans un champ)
@@ -54,9 +38,13 @@ jQuery( document ).ready( function( $ ) {
 
         // alert(xhr.responseText);
 
-        if ( xhr.responseText == '{"success":"success"}' ) {
+        $( "#newsletter-form" ).ajaxSuccess( function() {
             $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
         }
+
+        // if ( xhr.responseText == '{"success":"success"}' ) {
+        //     $( "#newsletter-form .et_bloom_form_header h2" ).addClass("subscribed");
+        // }
     });
 
     // $("#bulletin-column form").bind('ajax:complete', function() {
